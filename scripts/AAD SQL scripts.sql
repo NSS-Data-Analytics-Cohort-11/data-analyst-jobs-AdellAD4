cc8f0a5d75827665b8c73b5c8e52981b57cdff77
AdellAD4 <adaly4444@gmail.com>
2024-02-06 22:38:49 -0600

    Starting Bonus Questions

--- scripts/AAD SQL scripts.sql
+++ scripts/AAD SQL scripts.sql
@@ -35,20 +35,48 @@ WHERE review_count BETWEEN 500 AND 1000;
 --6.	Show the average star rating for companies in each state. The output should show the state as `state` and the average rating for the state as `avg_rating`. Which state shows the highest average rating?
 SELECT AVG(star_rating) AS avg_rating, location AS state
 FROM data_analyst_jobs
-GROUP BY state; 
---Answer NE **HOWEVER**, I would love to know why it would not allow me to use the DESC command in this query. I kept getting an error when I added "DESC" after state in my GROUP BY clause.
+GROUP BY state
+ORDER BY avg_rating DESC; 
+--Answer NE
 
 --7.	Select unique job titles from the data_analyst_jobs table. How many are there?
+SELECT COUNT (DISTINCT title)
+FROM data_analyst_jobs;
+--Answer 881
 
 --8.	How many unique job titles are there for California companies?
+SELECT COUNT (DISTINCT title)
+FROM data_analyst_jobs
+WHERE location = 'CA';
+--Answer 230
 
 --9.	Find the name of each company and its average star rating for all companies that have more than 5000 reviews across all locations. How many companies are there with more that 5000 reviews across all locations?
+SELECT company, AVG(star_rating) AS avg_star_rating
+FROM data_analyst_jobs
+WHERE review_count > 5000
+GROUP BY company;
+--Answer 41
 
 --10.	Add the code to order the query in #9 from highest to lowest average star rating. Which company with more than 5000 reviews across all locations in the dataset has the highest star rating? What is that rating?
+SELECT company, AVG(star_rating) AS avg_star_rating
+FROM data_analyst_jobs
+WHERE review_count > 5000
+GROUP BY company 
+ORDER BY avg_star_rating DESC;
+--Answer General Motors
 
 --11.	Find all the job titles that contain the word ‘Analyst’. How many different job titles are there? 
+SELECT DISTINCT (title)
+FROM data_analyst_jobs
+WHERE title iLIKE '%Analyst%';
+--Answer 774
 
 --12.	How many different job titles do not contain either the word ‘Analyst’ or the word ‘Analytics’? What word do these positions have in common?
+SELECT title
+FROM data_analyst_jobs
+WHERE title NOT LIKE '%Analyst%' 
+AND title NOT LIKE '%Analytics%';
+--Answer 39 **HOWEVER** most of these still contain the word ANALYST. (Am I doing something wrong?). Also most of them contain the word DATA.
 
 --**BONUS:**
 --You want to understand which jobs requiring SQL are hard to fill. Find the number of jobs by industry (domain) that require SQL and have been posted longer than 3 weeks. 
